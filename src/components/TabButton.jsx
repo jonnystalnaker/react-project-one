2bc16122fd6bf0a94b69a8e1e8072b46f08ec626
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-02 11:52:19 -0700

    finished project

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-﻿export default function TabButton({ children, onSelect, isSelected }) {
+﻿export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
       <button
-        className={isSelected ? 'active' : ''}
-        onClick={onSelect}
+        className={isSelected ? 'active' : undefined}
+        {...props}
       >
         {children}
       </button>
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,8 @@
+﻿export default function Tabs({ children, buttons, ButtonContainer = 'menu' }) {
+  return (
+    <>
+      <ButtonContainer>{buttons}</ButtonContainer>
+      {children}
+    </>
+  );
+}
